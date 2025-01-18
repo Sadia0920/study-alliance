28e62315588a7b43e912eb55042228f85724e6d4
Sadia0920 <sadiaafrin0920@gmail.com>
2025-01-18 18:28:50 +0600

    added a conditional disabled button to the session details route

--- src/pages/SessionCardDetails.jsx
+++ src/pages/SessionCardDetails.jsx
@@ -7,6 +7,10 @@ export default function SessionCardDetails() {
   const loadedSessionDetails = useLoaderData()
   
   const {_id,sessionTitle,tutorName,averageRating,sessionDescription,registrationStartDate,registrationEndDate,classStartTime,classEndDate,sessionDuration,registrationFee,reviews,bookNowStatus,additionalInfo} = loadedSessionDetails
+  const currentDate = new Date();
+ const endDate = new Date(registrationEndDate);
+//  const startDate = new Date(registrationStartDate);
+ const isOngoing = currentDate <= endDate;
   
   return (
     <div className='w-11/12 md:w-10/12 lg:w-7/12 mx-auto mt-36 mb-7'>
@@ -25,8 +29,25 @@ export default function SessionCardDetails() {
     <p className="text-lg mb-2 font-semibold text-gray-800">Class End Date :<span className='text-gray-500'>{classEndDate}</span></p>
     <p className="text-lg mb-2 font-semibold text-gray-800">Session Duration :<span className='text-gray-500'>{sessionDuration}</span></p>
     <p className="text-lg mb-2 font-semibold text-gray-800">Registration Fee :<span className='text-gray-500'>{registrationFee}</span></p>
-    <p className="text-lg mb-2 font-semibold text-gray-800">Book Now Status :<span className='text-gray-500'>{bookNowStatus}</span></p>
+    {isOngoing ? (
+        <p className="text-lg mb-2 font-semibold text-gray-800">Registration Status: <strong className='text-green-600'>Ongoing</strong></p>
+      ) : (
+        <p className="text-lg mb-2 font-semibold text-gray-800">Registration Status: <strong className='text-red-600'>Closed</strong></p>
+    )}
+    {/* <p className="text-lg mb-2 font-semibold text-gray-800">Book Now Status :<span className='text-gray-500'>{bookNowStatus}</span></p> */}
     <p className="text-lg mb-2 font-semibold text-gray-800">Additional Info :<span className='text-gray-500'>{additionalInfo}</span></p>
+    <div className="card-actions">
+      {
+        registrationFee == 'Free' ? (
+          <button disabled={true} className="btn bg-green-800 text-white">{bookNowStatus}</button>
+        )
+        :
+        (
+          <Link ><button className="btn bg-green-800 text-white">{bookNowStatus}</button></Link>
+        )
+      }
+      
+   </div>
 
 </div>
 </div>
