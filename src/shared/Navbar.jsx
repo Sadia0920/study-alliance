bf9fdc585e8e376f984848946befac11e03b61d5
Sadia0920 <sadiaafrin0920@gmail.com>
2025-05-01 12:53:54 +0600

    set the alignment of navbar and also make a custome screen size

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -21,7 +21,7 @@ export default function Navbar() {
     }
   return (
     <div className='bg-[#EAE6FF] text-[rgb(51,51,51)] bg-opacity-60 fixed top-0 z-10 w-full'>
-    <div className="navbar w-10/12 mx-auto">
+    <div className="navbar w-full md:w-10/12 mx-auto">
     <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -48,8 +48,8 @@ export default function Navbar() {
     </div>
 
 
-    <i className="fa-solid fa-book-open-reader text-2xl text-[rgb(76,48,161)]"></i>
-    <a className="btn btn-ghost text-xl px-1 text-[rgb(76,48,161)]">Study Alliance</a>
+    <i className="fa-solid fa-book-open-reader text-xl custom:text-2xl text-[rgb(76,48,161)]"></i>
+    <a className="btn btn-ghost custom:text-xl px-1 text-[rgb(76,48,161)]">Study Alliance</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
@@ -58,17 +58,17 @@ export default function Navbar() {
       <li className=' font-semibold text-[rgb(76,48,161)]'><NavLink to='/aboutUs'>About Us</NavLink></li>
     </ul>
   </div>
-  <div className="md:navbar-end w-7/12 ml-9 lg:ml-14">
+  <div className="md:navbar-end w-7/12 ml-[70px] lg:ml-14">
   {
       user?
       <>
       <a data-tooltip-id="my-tooltip"
   data-tooltip-content={user.displayName}
   data-tooltip-place="top">
-      <img className='w-7 h-7 md:w-10 md:h-10 rounded-full' src={user.photoURL} alt="" />
+      <img className='w-8 h-8 custom:w-9 custom:h-9 md:w-10 md:h-10 rounded-full' src={user.photoURL} alt="" />
       </a>
       <div className="dropdown dropdown-hover">
-  <div tabIndex={0} role="button" className="btn ml-3 bg-[rgb(76,48,161)] text-white p-3 md:p-4 rounded-full">
+  <div tabIndex={0} role="button" className="btn ml-1 custom:ml-3 bg-[rgb(76,48,161)] text-white rounded-full">
   <i className="fa-solid fa-bars"></i>
   </div>
   <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-32 p-2 shadow">
@@ -81,7 +81,7 @@ export default function Navbar() {
       <>
       <i className="fa-solid fa-user text-white p-3 rounded-full bg-[rgb(76,48,161)]"></i>
       <div className="dropdown dropdown-hover">
-  <div tabIndex={0} role="button" className="btn p-4 rounded-full ml-3 text-white bg-[rgb(76,48,161)]"><i className="fa-solid fa-bars"></i></div>
+  <div tabIndex={0} role="button" className="btn border-none rounded-full ml-1 custom:ml-3 text-white bg-[rgb(76,48,161)]"><i className="fa-solid fa-bars"></i></div>
   <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-32 p-2 shadow">
     <li><Link to='/login' className="btn text-white bg-[rgb(76,48,161)]">Login</Link></li>
     <li><Link to='/register' className="btn text-white bg-[rgb(76,48,161)]">Register</Link></li>
@@ -90,7 +90,7 @@ export default function Navbar() {
 
       </>
 }
-<div className='hidden md:block'>
+<div className='hidden custom:block'>
 <ThemeController></ThemeController>
 </div>
       
