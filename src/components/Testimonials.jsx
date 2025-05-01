6cb79ab494d9a44cfdd85d2b79ed49e6b314af9a
Sadia0920 <sadiaafrin0920@gmail.com>
2025-05-01 13:27:31 +0600

    change the testimonials section bg color

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -6,14 +6,14 @@ export default function Testimonials() {
     <div className=" grid grid-cols-1 gap-6">
     {/* 1 */}
     <h2 className="text-xl font-bold text-center"><i className="fa-solid fa-people-group text-3xl mr-2"></i>Student Story</h2>
-    <div className="card bg-[rgb(76,48,161,0.2)] shadow-xl text-left">
+    <div className="card bg-[rgb(212,208,229)] shadow-xl text-left">
       <div className="card-body">
       <h4 className="text-lg text-black font-bold">"The best online learning platform!"</h4>
-      <h5 className="text-black font-bold">Student Name - "John Doe"</h5>
-      <p className="font-medium text-black">"EduSphere helped me master AI fundamentals. The courses are engaging, and the instructors are highly knowledgeable. I landed my first internship thanks to this platform!"</p>
+      <h5 className="font-bold  text-black">Student Name - "John Doe"</h5>
+      <p className="font-medium  text-black">"EduSphere helped me master AI fundamentals. The courses are engaging, and the instructors are highly knowledgeable. I landed my first internship thanks to this platform!"</p>
       </div>
       </div>
-      <div className="card bg-[rgb(76,48,161,0.2)] shadow-xl text-left">
+      <div className="card bg-[rgb(212,208,229)] shadow-xl text-left">
       <div className="card-body">
       <h4 className="text-lg text-black font-bold">"Flexible learning at its best!"</h4>
       <h5 className="text-black font-bold">Student Name - "Michael Smith"</h5>
@@ -22,7 +22,7 @@ export default function Testimonials() {
   </div>
   {/* 2 */}
     <h2 className="text-xl font-bold text-center"><i className="fa-solid fa-user text-3xl mr-2"></i>Tutor Story</h2>
-  <div className="card bg-[rgb(76,48,161,0.2)] shadow-xl text-left">
+  <div className="card bg-[rgb(212,208,229)] shadow-xl text-left">
       <div className="card-body">
       <h4 className="text-lg text-black font-bold">"A game-changer for teachers!"</h4>
       <h5 className="text-black font-bold">Tutor Name - "Sarah Lee "</h5>
