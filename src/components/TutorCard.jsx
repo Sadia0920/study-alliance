233c4f344ac10bb89c93337e259e089bf0a05580
Sadia0920 <sadiaafrin0920@gmail.com>
2025-05-01 13:40:36 +0600

    change the tutor cards text color

--- src/components/TutorCard.jsx
+++ src/components/TutorCard.jsx
@@ -5,10 +5,10 @@ export default function TutorCard({data}) {
 const {tutorName,sessionTitle}=data
   return (
     <div className='courseCard'>
-      <h3 className="courseCard-title">{tutorName}</h3>
-      <h3 className="courseCard-title">Subjects:</h3>
+      <h3 className="courseCard-title text-black">{tutorName}</h3>
+      <h3 className="courseCard-title text-black">Subjects:</h3>
       {
-        sessionTitle.map((sub,idx)=> <li key={idx}>{sub}</li>)
+        sessionTitle.map((sub,idx)=> <li className='text-black' key={idx}>{sub}</li>)
       }
     </div>
 
